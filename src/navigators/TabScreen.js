1c1e90780c27cc6d30a3c8ce7e9a034050e20757
Arya Wibowo Jati <aryajati29@gmail.com>
2024-09-09 21:51:09 +0700

    feat: add page createJob

--- src/navigators/TabScreen.js
+++ src/navigators/TabScreen.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LogoutScreen from '../screens/LogoutScreen';
 import HomeClient from '../screens/HomeClient';
+import CreateJobScreen from '../screens/CreateJobScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export default function TabScreen(props) {
   return (
     <>
       <Tab.Navigator>
+        <Tab.Screen name="CreateJob" component={CreateJobScreen} />
         <Tab.Screen name="HomeClientScreen" component={HomeClient} />
         <Tab.Screen name="Logout" component={LogoutScreen} />
       </Tab.Navigator>
