28149cee2277f3d3b2bd363137d44077ea3f60f4
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-09-10 07:41:45 +0700

    feat: add tabnavigator icon

--- src/navigators/TabScreen.js
+++ src/navigators/TabScreen.js
@@ -2,16 +2,40 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LogoutScreen from '../screens/LogoutScreen';
 import HomeClient from '../screens/HomeClient';
 import ProfileScreen from '../screens/ProfileScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabScreen(props) {
-
   return (
     <>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeClient} />
-        <Tab.Screen name='Profile' component={ProfileScreen}/>
+        <Tab.Screen
+          name="Home"
+          component={HomeClient}
+          options={{
+            tabBarIcon: ({ color, size, focused }) =>
+              focused ? (
+                <Ionicons name="home" size={size} color="#1E204C" />
+              ) : (
+                <Ionicons name="home-outline" size={size} color="#1E204C" />
+              ),
+            tabBarShowLabel: false,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size, focused }) =>
+              focused ? (
+                <Ionicons name="person" size={size} color='#1E204C' />
+              ) : (
+                <Ionicons name="person-outline" size={size} color='#1E204C' />
+              ),
+            tabBarShowLabel: false,
+          }}
+        />
         <Tab.Screen name="Logout" component={LogoutScreen} />
       </Tab.Navigator>
     </>
