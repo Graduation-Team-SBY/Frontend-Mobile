2f359d8d90ce1f0549cacfcec80b378dc9c4f33a
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-09-10 13:15:57 +0700

    fix: change baseurl

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const instanceAxios = axios.create({
-  // baseURL: "http://localhost:3000/"
-  baseURL: "http://35.197.155.134",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "https://989f-103-165-209-195.ngrok-free.app",
+  // baseURL: "http://35.197.155.134",
 });
