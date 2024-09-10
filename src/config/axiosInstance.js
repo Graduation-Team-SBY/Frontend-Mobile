84a2d5f92ad54abbb997247b10a85c1fc773e949
Arya Wibowo Jati <aryajati29@gmail.com>
2024-09-10 14:44:56 +0700

    fix: baseUrl use deployed server

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const instanceAxios = axios.create({
   // baseURL: "http://localhost:3000/",
-  baseURL: "https://989f-103-165-209-195.ngrok-free.app",
-  // baseURL: "http://35.197.155.134",
+  // baseURL: "https://989f-103-165-209-195.ngrok-free.app",
+  baseURL: "http://35.197.155.134",
 });
