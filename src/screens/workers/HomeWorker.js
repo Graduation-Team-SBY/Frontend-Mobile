619401f6b613964a17b931624673141ef859d65b
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-09-12 00:28:45 +0700

    feat: add homeworker design

--- src/screens/workers/HomeWorker.js
+++ src/screens/workers/HomeWorker.js
@@ -1,26 +1,31 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { SignalIcon } from 'react-native-heroicons/solid';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+} from 'react-native';
 import { CurrencyDollarIcon } from 'react-native-heroicons/solid';
 import Swiper from 'react-native-swiper';
 import { instanceAxios as axios } from '../../config/axiosInstance';
-import * as SecureStore from "expo-secure-store";
+import * as SecureStore from 'expo-secure-store';
 import { formatCurrencyRupiah } from '../../helpers/currency';
+import NewestJobCard from '../../components/NewestJobCard';
 export default function HomeWorker({ navigation }) {
-  const [profile, setProfile] = useState({ name: "User" })
-  const [wallet, setWallet] = useState({ amount: 0 })
-  const [isloading, setIsloading] = useState(false)
+  const [profile, setProfile] = useState({ name: 'User' });
+  const [wallet, setWallet] = useState({ amount: 0 });
+  const [newestJob, setNewestJob] = useState([]);
+  const [isloading, setIsloading] = useState(false);
 
   const fetchProfile = async () => {
     try {
-      setIsloading(true)
-      let access_token = await SecureStore.getItemAsync("access_token")
+      setIsloading(true);
+      let access_token = await SecureStore.getItemAsync('access_token');
       const profileResponse = await axios({
         method: 'GET',
-        url: 'clients/profile',
+        url: 'workers/profile',
         headers: {
           Authorization: `Bearer ${access_token}`,
         },
@@ -33,88 +38,37 @@ export default function HomeWorker({ navigation }) {
           Authorization: `Bearer ${access_token}`,
         },
       });
-      setProfile(profileResponse.data)
-      setWallet(walletResponse.data)
+      setProfile(profileResponse.data);
+      setWallet(walletResponse.data);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     } finally {
-      setIsloading(false)
+      setIsloading(false);
     }
-  }
-
-  useEffect(() => {
-
-    fetchProfile()
-  }, [])
-
-  const [paymentUrl, setPaymentUrl] = useState(null);
-
-  const handlePayment = async () => {
+  };
+  const fethNewestJob = async () => {
     try {
-      // Call your backend to get the payment token
-      console.log('jalan');
-      const token = await getItemAsync('access_token');
+      let access_token = await SecureStore.getItemAsync('access_token');
       const { data } = await axios({
-        method: 'POST',
-        url: '/payment/topup',
+        method: 'GET',
+        url: 'workers/jobs/worker/newest',
         headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        data: {
-          amount: Number(10000),
+          Authorization: `Bearer ${access_token}`,
         },
       });
 
-      const redirect_url = `https://app.sandbox.midtrans.com/snap/v2/vtweb/${data.trans_token}`;
-
-      setPaymentUrl(redirect_url);
-    } catch (error) {
-      console.log(error.response.data.message);
+      setNewestJob(data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsloading(false);
     }
   };
 
-  // return (
-  //   <View style={{ flex: 1 }}>
-  //     {paymentUrl ? (
-  //       <WebView
-  //         source={{ uri: paymentUrl }}
-  //         onNavigationStateChange={async (navState) => {
-  //           if (navState.url.includes('transaction-status=success')) {
-  //             // Handle payment success
-  //             const token = await getItemAsync('access_token');
-  //             await axios({
-  //               method: 'patch',
-  //               url: '/profile/wallet',
-  //               headers: {
-  //                 Authorization: `Bearer ${token}`,
-  //               },
-  //               data: {
-  //                 topupId: data.topupId,
-  //               },
-  //             });
-
-  //             await fetchWallet();
-  //             setAmount(null);
-  //             // toast.success('Payment Success!');
-  //             console.log('Payment Success!');
-  //           } else if (navState.url.includes('transaction-status=pending')) {
-  //             // Handle payment pending
-  //             setAmount(null);
-  //             // toast.info('Menunggu pembayaran Anda!');
-  //             console.log('Payment Waiting!');
-  //           } else if (navState.url.includes('transaction-status=error')) {
-  //             // Handle payment error
-  //             setAmount(null);
-  //             // toast.error('Pembayaran Gagal!');
-  //             console.log('Payment Gagal!');
-  //           }
-  //         }}
-  //       />
-  //     ) : (
-  //       <Button title="Pay Now" onPress={handlePayment} />
-  //     )}
-  //   </View>
-  // );
+  useEffect(() => {
+    fethNewestJob();
+    fetchProfile();
+  }, []);
   return (
     <ScrollView style={{ backgroundColor: '#FAF9FE' }}>
       <View style={styles.container}>
@@ -135,51 +89,13 @@ export default function HomeWorker({ navigation }) {
               {formatCurrencyRupiah(wallet?.amount)}
             </Text>
           </View>
-          <View style={styles.ewalletJustify}>
-            <View style={styles.ewalletButton}>
-              <TouchableOpacity style={styles.topUpLogo}>
-                <FontAwesome name="credit-card" size={20} color="black" />
-                <Text>Top Up</Text>
-              </TouchableOpacity>
-              <View style={styles.topUpLogo}>
-                <FontAwesome5 name="arrow-circle-up" size={20} color="black" />
-                <Text>Transfer</Text>
-              </View>
-            </View>
-            <SignalIcon color={'#FFF'} size={32} />
-          </View>
         </View>
         <View>
-          <Text style={styles.title}>
-            Halo, <Text style={styles.span}>{profile?.name}</Text> !
+          <Text style={styles.titleWelcome}>
+            Selamat datang, <Text style={styles.span}>{profile?.name}</Text> !
           </Text>
         </View>
-        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-          Mau ngapain hari ini?
-        </Text>
-        <View style={styles.containerCard}>
-          <TouchableOpacity
-            style={styles.card}
-            onPress={() => navigation.navigate('CreateNitip')}
-          >
-            <Image
-              source={require('../../assets/shopping-bag.png')}
-              style={{ width: 50, height: 50 }}
-            />
-            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Nitip</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.card}
-            onPress={() => navigation.navigate('CreateBebersih')}
-          >
-            <Image
-              source={require('../../assets/bucket.png')}
-              style={{ width: 50, height: 50 }}
-            />
-            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Bebersih</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.containerSwiper}>
+        {/* <View style={styles.containerSwiper}>
           <Swiper
             autoplay
             autoplayTimeout={3} // delay 3 detik
@@ -213,7 +129,20 @@ export default function HomeWorker({ navigation }) {
               />
             </View>
           </Swiper>
+        </View> */}
+        <View
+          style={styles.quotes}
+        >
+          <Text style={{ fontSize: 24, fontWeight: '600' }}>YangTu</Text>
+          <Text style={{ color: '#4b5563', fontSize: 14 }}>
+            Bekerja yang didasarkan pada pekerjaan sementara, tugas - tugas
+            jangka pendek, atau kontrak pekerjaan yang fleksibel
+          </Text>
         </View>
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Pekerjaan Terbaru</Text>
+        { newestJob.map((val, i) => {
+          return <NewestJobCard key={i} job={val}/>
+        }) }
       </View>
     </ScrollView>
   );
@@ -233,24 +162,7 @@ const styles = StyleSheet.create({
     gap: 20,
     justifyContent: 'space-between',
   },
-  card: {
-    flex: 1,
-    borderRadius: 12,
-    gap: 12,
-    backgroundColor: '#FFFFFF',
-    height: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.41,
-    elevation: 2,
-  },
-  title: {
+  titleWelcome: {
     fontSize: 36,
     fontWeight: 'bold',
     color: '#1D204C',
@@ -262,30 +174,12 @@ const styles = StyleSheet.create({
   },
   textUser: {
     color: 'white',
-    fontSize: 16,
-  },
-  heading: {
     fontSize: 20,
-    fontWeight: 'bold',
   },
   textHeadingEwallet: {
     color: 'white',
     fontSize: 24,
     fontWeight: 'bold',
-  },
-  ewalletButton: {
-    backgroundColor: '#FFF',
-    flexDirection: 'row',
-    gap: 32,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderRadius: 50,
-    justifyContent: 'space-between',
-  },
-  topUpLogo: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 4,
   },
   ewallet: {
     backgroundColor: 'transparent',
@@ -335,4 +229,11 @@ const styles = StyleSheet.create({
     right: 0,
     borderRadius: 12,
   },
+  quotes: {
+    backgroundColor: '#DBEAFE',
+    padding: 16,
+    borderRadius: 12,
+    justifyContent: 'center',
+    gap: 4,
+  }
 });
